59ed0a8caa7a40f968ab653b50b305a7f31e31d6
tejprakash <tejprakash1590@gmail.com>
2019-07-01 16:07:22 -0500

    Setup private and public route

--- src/app.js
+++ src/app.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { login, logout } from './actions/auth';
 import './styles/style.scss';
 import 'normalize.css/normalize.css';
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
-// store.dispatch(addExpense({description: 'Rent', amount: 100, createdAt: 5000}));
-// store.dispatch(addExpense({description: 'Electricity', amount: 1000, createdAt: 1000}));
-// store.dispatch(addExpense({description: 'Water Bill', amount: 30, createdAt: 1200}));
-// store.dispatch(setTextFilter(''));
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
-)
+);
+
+let hasRendered = false;
+const renderApp = () => {
+    if(!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app'));
+        hasRendered = true;
+    }
+};
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app'));
+firebase.auth().onAuthStateChanged((user) => {
+    if(user) {
+        store.dispatch(login(user.id));
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+            if(history.location.pathname === '/') {
+                history.push('/dashboard');
+            }
+        });
+    } else {
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+    }
 });
 
 
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, NavLink  } from 'react-router-dom';
-import Header from '../components/Header';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory as createHistory } from 'history';
 import ExpenseDashboard from '../components/ExpenseDashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
-import Help from '../components/Help';
 import NotFound from '../components/NotFound';
+import LoginPage  from '../components/Login';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
+
+export const history = createHistory();
 
 const AppRouter = () => (
-    <Router>
+    <Router history={history}>
         <div>
-            <Header />
             <Switch>
-                <Route path="/" component={ExpenseDashboard} exact />
-                <Route path="/create" component={AddExpense} />
-                <Route path="/edit/:id" component={EditExpense} />
-                <Route path="/help" component={Help} />
+                <PublicRoute path="/" component={LoginPage} exact={true} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboard} />
+                <PrivateRoute path="/create" component={AddExpense} />
+                <PrivateRoute path="/edit/:id" component={EditExpense} />
                 <Route component={NotFound} />
             </Switch>
         </div>
@@ -23,4 +26,6 @@ const AppRouter = () => (
 );
 
 export default AppRouter;
+
+
 
